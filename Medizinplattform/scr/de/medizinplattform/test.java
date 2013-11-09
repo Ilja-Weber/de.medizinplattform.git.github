41ef41dbd36bc8d8f0775b5ae273e8f8867a9e28
Ilja Weber <ilja1989weber@googlemail.com>
2013-11-09 19:46:36 +0100

    Testen des Commits

--- Medizinplattform/scr/de/medizinplattform/test.java
+++ Medizinplattform/scr/de/medizinplattform/test.java
@@ -3,7 +3,7 @@ package de.medizinplattform;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, koennt ihr mich sehen?");
+		System.out.println("Hi, koennt ihr mich sehen? v2.0");
 	}
 
 }
