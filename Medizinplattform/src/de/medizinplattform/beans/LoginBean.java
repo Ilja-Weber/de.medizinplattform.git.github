0bff05f72d07f98e70d8e2d45264665925dd5cdf
Ilja Weber <ilja1989weber@googlemail.com>
2013-11-23 21:46:15 +0100

    writing some code for loginbean and fixing some bugs

--- Medizinplattform/src/de/medizinplattform/beans/LoginBean.java
+++ Medizinplattform/src/de/medizinplattform/beans/LoginBean.java
@@ -1,5 +1,22 @@
 package de.medizinplattform.beans;
 
-public class LoginBean {
 
+public class LoginBean {
+	private String name = "Enter your name here";
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(final String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+	
+	
 }
