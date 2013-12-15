0d5e530bfe53c6804ccb6895029332b43a8a5b83
Ilja Weber <ilja1989weber@googlemail.com>
2013-12-15 15:00:42 +0100

    add Entry entity

--- Medizinplattform/src/de/medizinplattform/entities/Entry.java
+++ Medizinplattform/src/de/medizinplattform/entities/Entry.java
@@ -1,39 +1,76 @@
 package de.medizinplattform.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ENTITY")
 public class Entry {
 	
-	public String chronicleId;
-	public String text;
-	public boolean editable=false;
+	
+	//VARIABLE - OUTER
+	@Id @GeneratedValue
+	private long id;
+	public long getId(){
+		return id;
+	}
+	public void setId(long newId){
+		this.id=newId;
+	}
 	
 	
-	public Entry(String chronicleId, String text){
-		this.chronicleId=chronicleId;
-		this.text=text;
+	//VARIABLE - OUTER
+	private String belongsToStory;
+	public String getBelongsToStory(){
+		return belongsToStory;
+	}
+	public void setBelongsToStory(String newStoryId){
+		this.belongsToStory=newStoryId;
+	}
+	
+	//VARIABLE - OUTER
+	private String type;
+	public String getType(){
+		return type;
+	}
+	public void setType(String newType){
+		this.type=newType;
+	}
+	
+	//VARIABLE - OUTER
+	private String date; //Format dd.mm.yyyy
+	public String getDate(){
+		return date;
+	}
+	public void setDate(String newDate){
+		this.date=newDate;
+	}
+	public void setDate(String dd, String mm, String yyyy){
+		this.date=dd+"."+mm+"."+yyyy;
 	}
 	
 	
-	public String getText() {
-		return text;
+	//VARIABLE - OUTER
+	private String time; //Format hh:mm
+	public String getTime(){
+		return time;
 	}
-	public void setText(String text) {
-		this.text = text;
+	public void setTime(String newTime){
+		this.time=newTime;
 	}
-	public boolean isEditable() {
-		return editable;
-	}
-	public void setEditable(boolean editable) {
-		this.editable = editable;
-	}
-	public String getChronicleId() {
-		return chronicleId;
-	}
-	public void setChronicleId(String chronicleId) {
-		this.chronicleId = chronicleId;
+	public void setTime(String hh, String mm){
+		this.time=hh+":"+mm;
 	}
 	
-	@Override
-	public String toString(){
-		return chronicleId;
+	
+	//VARIABLE - OUTER
+	private String content;
+	public String getContent(){
+		return content;
+	}
+	public void setContent(String newContent){
+		this.content=newContent;
 	}
 }
