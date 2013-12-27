dff93fbfc0cf9d72a9a67acdfb2a43aef2ac98ba
Ilja Weber <ilja1989weber@googlemail.com>
2013-12-27 18:17:28 +0100

    delete some unused methods

--- Medizinplattform/src/de/medizinplattform/managedbeans/ChronicleBean.java
+++ Medizinplattform/src/de/medizinplattform/managedbeans/ChronicleBean.java
@@ -1,21 +1,10 @@
 package de.medizinplattform.managedbeans;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
 
-import de.medizinplattform.entities.Story;
-import de.medizinplattform.entities.User;
 import de.medizinplattform.managedbeans.components.NewStoryComponent;
-import de.medizinplattform.managedbeans.components.StoryComponent;
-import de.medizinplattform.managedbeans.components.UserBeanComponent;
 
 @ManagedBean(name = "chronicleBean")
 @SessionScoped
@@ -49,64 +38,6 @@ public class ChronicleBean {
 		}
 		return newStoryC;
 	}
-
-	// Variable - OUTER
-	private List<StoryComponent> storiesC;
-
-	public List<StoryComponent> getStoriesC() {
-		if (storiesC == null) {
-			storiesC = new ArrayList<StoryComponent>();
-			List<Story> storiesList = new ArrayList<Story>();
-
-			// Get list of all users
-			// get entitymanager
-			EntityManagerFactory emf = Persistence
-					.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-			EntityManager em = emf.createEntityManager();
-			// Create a query
-			Query q = em.createQuery("SELECT x FROM Story x");
-			storiesList = (List<Story>) q.getResultList();
-
-			// In for-loop create for each User a userComponent
-			for (Story story : storiesList) {
-				storiesC.add(new StoryComponent(this, story));
-			}
-		}
-		return storiesC;
-	}
-
-	// Variable - OUTER
-	public boolean isNewStoryButtonVisible() {
-		return getNewStoryComponent().isNewStoryButtonVisible();
-
-	}
-
-	// Variable - OUTER
-	public boolean isOptionsVisible() {
-		return getNewStoryComponent().isOptionsVisible();
-	}
-
-	// Variable - OUTER
-	public boolean isSymptomVisible() {
-		return getNewStoryComponent().isSymptomVisible();
-	}
-
-	// Buttons logic
-	public String gotoOptions() {
-		getNewStoryComponent().gotoOptions();
-		return "new-story.xhtml?faces-redirect=true";
-	}
-
-	// Logic
-	public String gotoSymptomAnket() {
-		getNewStoryComponent().gotoSymptomAnket();
-		return null;
-	}
-
-	// Buttons logic
-	public String cancel() {
-		getNewStoryComponent().cancel();
-		return null;
-	}
+	
 
 }
--- Medizinplattform/src/de/medizinplattform/managedbeans/components/NewStoryComponent.java
+++ Medizinplattform/src/de/medizinplattform/managedbeans/components/NewStoryComponent.java
@@ -23,53 +23,7 @@ public class NewStoryComponent {
 	//Constructor
 	public NewStoryComponent(ChronicleBean cronicleBean){
 		this.parent=cronicleBean;
-		cancel();
 	}
 	
-	
-	
-	
-	
-	//Variable - OUTER
-	private boolean newStoryButtonVisible;
-	public boolean isNewStoryButtonVisible(){
-		return newStoryButtonVisible;
-	}
-	
-	//Variable - OUTER
-	private boolean optionsVisible;
-	public boolean isOptionsVisible(){
-		return optionsVisible;
-	}
-	
-	//Variable - OUTER
-	private boolean symptomVisible;
-	public boolean isSymptomVisible(){
-		return symptomVisible;
-	}
-	
-	//Buttons logic	
-	public void gotoOptions(){
-		reset();
-		optionsVisible=true;
-	}
-	
-	//Buttons logic	
-	public void cancel(){
-		reset();
-		newStoryButtonVisible=true;
-	}
-	
-	//Logic
-	public void gotoSymptomAnket(){
-		reset();
-		symptomVisible=true;
-	}
-	
-	public void reset(){
-		newStoryButtonVisible=false;
-		optionsVisible=false;
-		symptomVisible=false;
-	}
-	
+
 }
