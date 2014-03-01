21bece7b820495cc59086869930ad1fa6cd6713b
Ilja Weber <ilja1989weber@googlemail.com>
2014-03-01 12:46:06 +0100

    cleaning

--- Medizinplattform/src/de/medizinplattform/managedbeans/ChronicleBean.java
+++ Medizinplattform/src/de/medizinplattform/managedbeans/ChronicleBean.java
@@ -11,6 +11,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -20,6 +21,7 @@ import org.primefaces.event.CloseEvent;
 
 import de.medizinplattform.entities.Entry;
 import de.medizinplattform.entities.Story;
+import de.medizinplattform.entities.User;
 import de.medizinplattform.utilitybeans.DateUtility;
 
 @ManagedBean(name = "chronicleBean")
@@ -39,105 +41,14 @@ public class ChronicleBean {
 	private final String PERSISTENCE_UNIT_NAME = "common-entities";
 	
 	
-	//Variable - INNER
-	private HashMap<String, Integer> month_days;
+
 
 	// Constructor
 	public ChronicleBean() {
-		month_days = new HashMap<String, Integer>();
-		month_days.put("alle", 31);
-		month_days.put("Januar", 31);
-		month_days.put("Februar", 28);
-		month_days.put("März", 31);
-		month_days.put("April", 30);
-		month_days.put("Mai", 31);
-		month_days.put("Juni", 30);
-		month_days.put("Juli", 31);
-		month_days.put("August", 31);
-		month_days.put("September", 30);
-		month_days.put("October", 31);
-		month_days.put("November", 30);
-		month_days.put("Dezember", 31);
-		
 		resetVisibility();
 		listOfStoriesVisible=true;
 	}
-	
-	//Variable - OUTER
-	private List<String> years;
-	public List<String> getYears(){
-		if(years==null){
-			years=new ArrayList<String>();
-			int starting_year = (int) DateUtility.calculateYear();
-			for(int i=0; i<13; i++){
-				years.add(0,String.valueOf(starting_year-i));
-			}
-			years.add(0, "alle");
-		}
-		return years;
-	}
-	
-	//Variable - OUTER
-	private List<String> months;
-	public List<String> getMonths(){
-		if(months==null){
-			months=new ArrayList<String>();
-			months.add("alle");
-			months.add("Januar");
-			months.add("Februar");
-			months.add("März");
-			months.add("April");
-			months.add("Mai");
-			months.add("Juni");
-			months.add("Juli");
-			months.add("August");
-			months.add("September");
-			months.add("October");
-			months.add("November");
-			months.add("Dezember");
-		}
-		return months;
-	}
 
-	//Variable - OUTER
-	private List<String> days;
-	public List<String> getDays(){
-		if(days==null){
-			days=new ArrayList<String>();
-			int limit=month_days.get(getMonths().get(selectedMonth));
-			for(int i=1; i<=limit; i++){
-				days.add(String.valueOf(i));
-			}
-			days.add(0, "alle");
-		}
-		return days;
-	}
-	
-	//Variable - OUTER
-	private int selectedYear=0;
-	public String selectYear(int index){
-		System.out.println("Year selected: "+index);
-		selectedYear=index;
-		return null;
-	}
-	
-	//Variable - OUTER
-	private int selectedMonth=0;
-	public String selectMonth(int index){
-		System.out.println("Month selected: "+index);
-		selectedMonth=index;
-		days=null;
-		return null;
-	}
-	
-	//Variable - OUTER
-	private int selectedDay=0;
-	public String selectDay(int index){
-		System.out.println("Day selected: "+index);
-		selectedDay=index;
-		return null;
-	}
-	
 	//Logic
 	public void createStory(){
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
@@ -158,18 +69,13 @@ public class ChronicleBean {
 			long year = DateUtility.calculateYear();		
 			toBeCreated.setF_year(year);
 			toBeCreated.setT_year(year);
-			selectYear(years.size()-1);
 			
 			long month = DateUtility.calculateMonth();		
 			toBeCreated.setF_month(month);
 			toBeCreated.setT_month(month);
-			selectMonth((int) month);
-			
 			long day = DateUtility.calculateDay();		
 			toBeCreated.setF_day(day);
 			toBeCreated.setT_day(day);
-			selectDay((int) day);
-			
 			long hour = DateUtility.calculateHour();		
 			toBeCreated.setF_hour(hour);
 			toBeCreated.setT_hour(hour);
@@ -389,9 +295,7 @@ public class ChronicleBean {
 	}
 	
 	public void handleClose(CloseEvent event) {  
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Panel Closed", "Closed panel id:'" + event.getComponent().getId() + "'");  
         deselectStory();
-        addMessage(message);
         try {
 			FacesContext.getCurrentInstance().getExternalContext().redirect("chronicle.xhtml");
 		} catch (IOException e) {
@@ -407,5 +311,35 @@ public class ChronicleBean {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);  
         FacesContext.getCurrentInstance().addMessage(null, message);  
     }
+	
+	public void deleteStory(Story story) {  
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		EntityManager em = emf.createEntityManager();
+		
+		System.out.println("lalala"+story.getTitle());
+		List<Entry> allEntries = null;		
+		Query q = em.createQuery("SELECT x FROM Entry x WHERE x.belongs_to_story = "+story.getId()+"");
+		allEntries = (List<Entry>) q.getResultList();
+		
+		for(Entry entry : allEntries){
+			em.getTransaction().begin();
+			Entry toDelete = em.merge(entry);
+    		em.remove(toDelete);
+    		em.getTransaction().commit();
+		}
+		
+		em.getTransaction().begin();
+		Story toDelete = em.merge(story);
+		em.remove(toDelete);
+		em.getTransaction().commit();
+		
+        FacesMessage msg;
+        msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Geschichte \"" + story.getTitle() + "\" gelöscht", "");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+	
+	public String getColorForStory(Story story){
+		return (story.getState().equals("running"))?"#ffd8d1" : "#d0e3b0";
+	}
 		
 }
